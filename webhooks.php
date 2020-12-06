55403da9a0b492f87131308fe364e5be69737e17
systemdevelop066383319 <75550563+systemdevelop066383319@users.noreply.github.com>
2020-12-07 02:39:36 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,5 +1,6 @@
 <?php // callback.php
-
+/*Return HTTP Request 200*/
+http_response_code(200);
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
