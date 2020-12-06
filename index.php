a530fc01e0057e9f1b093340545d8119938a393c
systemdevelop066383319 <75550563+systemdevelop066383319@users.noreply.github.com>
2020-12-07 02:43:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
 <?php
-	/*Return HTTP Request 200*/
-	http_response_code(200);
-?>
+/*Return HTTP Request 200*/
+http_response_code(200);
+
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+
+$access_token = 'DrlozA4wrnAg1YBXJ6hGMiB+nNxv0IThrvg9IZU4T5/Swzp1U/jSuZfaYFNABu4PTw6b28Lo1e5Tu1HYQ5hJJ43n+5HzXsbPn3Ghh58IQwxq2U62Bkh8uR9S68OxA+YX63/Sdm11NJeJ6q2aPRjKpAdB04t89/1O/w1cDnyilFU=';
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['source']['userId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
